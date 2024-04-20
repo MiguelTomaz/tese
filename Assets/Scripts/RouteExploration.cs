00ee758acde46ff18dafe791f9012757f23025ef
MiguelTomaz <migueltomaz.c@gmail.com>
2024-04-20 20:52:32 +0100

    [Exploration][Add] take photo

--- Assets/Scripts/RouteExploration.cs
+++ Assets/Scripts/RouteExploration.cs
@@ -9,6 +9,7 @@ using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 using Google.XR.ARCoreExtensions;
 using TMPro;
+using System.Security.Cryptography;
 
 public class RouteExploration : MonoBehaviour
 {
@@ -16,14 +17,17 @@ public class RouteExploration : MonoBehaviour
     public GameObject prefabLivraria;
     public GameObject prefabUni;
     public GameObject prefabClerigos;
+    public GameObject addDescriptionPhotoPainel;
 
     public GameObject arrowNorth;
     public GameObject arrowPoi;
     public TextMeshProUGUI northText;
     public TextMeshProUGUI poiDistanceText;
-
+    public TMP_InputField photoDescriptionInputField;
+    public TextMeshProUGUI poiDistancePointerText;
     public TextMeshProUGUI coordinatesLatPoiTeste;
     public TextMeshProUGUI coordinatesLongPoiTeste;
+    public TextMeshProUGUI clickPhotoTest;
 
     private bool canStartExploration = false;
 
@@ -99,10 +103,15 @@ public class RouteExploration : MonoBehaviour
     public Button addOrderPoiTesteBtn;
     public Button openRouteExplorationDetailsPainelBtn;
     public Button closeRouteExplorationDetailsPainelBtn;
+    public Button savePhotoButton;
+    public Button takePhotoButton;
+    public Button closeTakePhotoButton;
     private bool isEplorationBegin = false;
     private string apiUrlRouteDetails = "http://13.60.19.19:3000/api/route/details/";
     private List<PointOfInterest> poiListWithOrder = new List<PointOfInterest>();
     private string savedLanguage = "";
+    private int touristRouteId;
+
     private int routePoiCurrentOrder = 0;
     public Text exploringRouteText;
     public Text RouteName;
@@ -127,6 +136,7 @@ public class RouteExploration : MonoBehaviour
     public Text nextPoiDistance;
     void Start()
     {
+        addDescriptionPhotoPainel.SetActive(false);
         // Ativa a bússola
         Input.location.Start();
         Input.compass.enabled = true;
@@ -139,12 +149,21 @@ public class RouteExploration : MonoBehaviour
         savedLanguage = PlayerPrefs.GetString("Language", "en");
         int touristId = PlayerPrefs.GetInt("Current_Logged_TouristID", -1); // -1 é o valor padrão se a chave "UserID" não existir
         int choosenRoute = PlayerPrefs.GetInt("choosenRoute", -1); // -1 é o valor padrão se a chave "UserID" não existir
+
+        touristRouteId = PlayerPrefs.GetInt("touristic_route", -1);
+
         GetRouteDetails(choosenRoute);
 
         openRouteExplorationDetailsPainelBtn.onClick.AddListener(openPainelRouteExploration);
         closeRouteExplorationDetailsPainelBtn.onClick.AddListener(closePainelRouteExploration);
         addOrderPoiTesteBtn.onClick.AddListener(addRoutePoiOrder);
         VerifyGeospatialSupport();
+
+        takePhotoButton.onClick.RemoveListener(TakePhoto);
+        takePhotoButton.onClick.AddListener(TakePhoto);
+
+        closeTakePhotoButton.onClick.RemoveListener(closeTakePhoto);
+        closeTakePhotoButton.onClick.AddListener(closeTakePhoto);
     }
 
     // Update is called once per frame
@@ -198,7 +217,7 @@ public class RouteExploration : MonoBehaviour
             }
 
             double currentDistance = CalculateDistance(y1, x1, y2, x2);
-            currentPoiDistance.text = currentDistance + " m";
+            poiDistancePointerText.text = currentDistance + " m";
 
             double delta_x = x2 - x1;
             double delta_y = y2 - y1;
@@ -506,5 +525,170 @@ public class RouteExploration : MonoBehaviour
             Debug.Log("TrackingState.None");
             Invoke("PlaceObjects", 5.0f);
         }
+    }
+
+    public void TakePhoto()
+    {
+        clickPhotoTest.text = "clicou photo";
+        
+        StartCoroutine(Photo());
+        addDescriptionPhotoPainel.SetActive(true);
+    }
+
+    public IEnumerator Photo()
+    {
+        clickPhotoTest.text = "clicou photo 1";
+        Debug.Log("StartCoroutine");
+        yield return new WaitForEndOfFrame();
+        Camera camera = Camera.main;
+        int width = Screen.width;
+        int height = Screen.height;
+
+        clickPhotoTest.text = "clicou photo 2";
+
+        RenderTexture rt = new RenderTexture(width, height, 24);
+        camera.targetTexture = rt;
+        // The Render Texture in RenderTexture.active is the one
+        // that will be read by ReadPixels.
+        var currentRT = RenderTexture.active;
+        RenderTexture.active = rt;
+
+        // Render the camera's view.
+        camera.Render();
+        clickPhotoTest.text = "clicou photo 3";
+
+        // Make a new texture and read the active Render Texture into it.
+        Texture2D image = new Texture2D(width, height);
+        image.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+        image.Apply();
+        Debug.Log("image: " + image);
+        camera.targetTexture = null;
+        // Replace the original active Render Texture.
+        RenderTexture.active = currentRT;
+
+        //savePhoto(image, rt);
+        //savePhotoButton.onClick.AddListener(() => savePhoto(image, rt));
+        clickPhotoTest.text = "clicou photo 4";
+        byte[] bytes = image.EncodeToPNG();
+        Debug.Log("bytes: " + bytes);
+        string base64String = Convert.ToBase64String(bytes);
+        //System.IO.File.WriteAllText("base64.txt", base64String);
+        //string filePathBase64 = @"D:\uni\tese\rep\base64\base64.txt"; // Caminho completo do arquivo de texto
+        //System.IO.File.WriteAllText(filePathBase64, base64String);
+        string filename = DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".png";
+        Debug.Log("filename: " + filename);
+        string imageHash = CalculateImageHash(image);
+        Debug.Log("imageHash: " + imageHash);
+        //string filePath = Application.persistentDataPath + "/" + filename;
+        //string directoryName = "PhotosTaken";
+
+        //string directoryPath = @"D:\uni\tese\rep\photos_teste";
+
+        // Combine o caminho do diretório com o caminho do arquivo.
+        //string filePath = Path.Combine("Assets", directoryPath, DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".png");
+        //Debug.Log("filePath: " + filePath);
+        //System.IO.File.WriteAllBytes(filePath, bytes);
+
+
+        //addDescriptionPhotoPainel.SetActive(true);
+        //SubmitPhotoWithDescription(imageHash, filename, base64String);
+
+        clickPhotoTest.text = "clicou photo 5";
+        savePhotoButton.onClick.AddListener(() => { SubmitPhotoWithDescription(imageHash, filename, base64String); });
+
+        Destroy(rt);
+        Destroy(image);
+
+    }
+
+    public void SubmitPhotoWithDescription(string imageHash, string filename, string base64String)
+    {
+        clickPhotoTest.text = "clicou photo 6";
+        //send this to save descriptino painel button
+        int touristicRouteId = touristRouteId;
+        string description = photoDescriptionInputField.text;
+        if(string.IsNullOrEmpty(description))
+        {
+            description = "isNull";
+        }
+        DateTime date = DateTime.Now;
+
+        /**
+        if (!string.IsNullOrEmpty(description))
+        {
+            
+        }
+        else
+        {
+            Image buttonImage = savePhotoButton.GetComponent<Image>();
+            Color newColor = new Color(0f, 0.35f, 0.16f);
+            buttonImage.color = newColor;
+            //savePhotoButton.interactable = false;
+        }
+        */
+        clickPhotoTest.text = "clicou photo 7";
+        UploadPhoto(touristicRouteId, description, date, imageHash, filename, base64String);
+        addDescriptionPhotoPainel.SetActive(false);
+    }
+
+    string CalculateImageHash(Texture2D image)
+    {
+        byte[] imageBytes = image.EncodeToPNG();
+        string hash;
+
+        using (MD5 md5 = MD5.Create())
+        {
+            byte[] hashBytes = md5.ComputeHash(imageBytes);
+            hash = BitConverter.ToString(hashBytes).Replace("-", "").ToLowerInvariant();
+        }
+
+        return hash;
+    }
+
+    public void UploadPhoto(int touristRouteAssociationId, string description, DateTime date, string imageHash, string filename, string base64String)
+    {
+        clickPhotoTest.text = "clicou photo 8";
+        string url = "http://13.60.19.19:3000/api/photo/upload";
+
+        // Convertendo a imagem para bytes
+        Debug.Log("UploadPhoto: ");
+        // Criando o objeto de dados a serem enviados
+        WWWForm form = new WWWForm();
+        form.AddField("tourist_route_association_id", touristRouteAssociationId.ToString());
+        form.AddField("description", description);
+        form.AddField("date", date.ToString("yyyy-MM-ddTHH:mm:ss"));
+        form.AddField("image_hash", imageHash);
+        form.AddField("filename", filename);
+        form.AddField("image_base64", base64String);
+
+        clickPhotoTest.text = "clicou photo 9";
+
+        // Enviando a requisição
+        StartCoroutine(SendRequestPhoto(url, form));
+    }
+
+    IEnumerator SendRequestPhoto(string url, WWWForm form)
+    {
+        clickPhotoTest.text = "clicou photo 10";
+        using (UnityWebRequest www = UnityWebRequest.Post(url, form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError("Erro ao enviar a foto: " + www.error);
+                clickPhotoTest.text = "clicou photo 11";
+            }
+            else
+            {
+                clickPhotoTest.text = "foto enviada";
+                Debug.Log("Foto enviada com sucesso!");
+            }
+        }
+    }
+    
+    void closeTakePhoto()
+    {
+        addDescriptionPhotoPainel.SetActive(false);
     }
 }
